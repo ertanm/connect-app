1dc84b59d1033768fbe29fbd7da796070736994d
highpriesst <ertansofia@gmail.com>
2023-03-23 17:31:25 +0200

    bug: port issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,34 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import config from "config";
+import logger from "./utils/logger";
+import { version } from "../package.json";
+
+// import socket from "./socket";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
 
 const app = express();
 
-const port = 4000;
+const httpServer = createServer(app);
 
-app.listen(port, () => console.log(`listening on port:${port}`)); //Works
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: corsOrigin,
+//     credentials: true,
+//   },
+// });
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${version}`)
+);
+
+httpServer.listen(port, host, () => {
+  logger.info(`🚀 Server version ${version} is listening 🚀`);
+  logger.info(`http://${host}:${port}`);
+
+  //   socket({ io });
+});
