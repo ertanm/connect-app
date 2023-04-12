96ed5822a88791b6cc003c98fbf3ebb68c99b178
highpriesst <ertansofia@gmail.com>
2023-04-12 23:48:16 +0300

    Little style changes...

--- client/components/ChatRooms.tsx
+++ client/components/ChatRooms.tsx
@@ -1,4 +1,3 @@
-//this is for creating a chat room
 import { useRef } from "react";
 import { useSockets } from "@/context/socketContext";
 
@@ -25,16 +24,20 @@ function ChatRooms() {
 
   return (
     <div className="flex flex-row gap-10">
-      <div className="border h-max">
+      <div className="border h-max bg-gray-100 p-4 rounded-lg">
         <ul>
           {Object.keys(chatRooms).map((key) => {
             return (
-              <div key={key} className="mb-10 border">
+              <div key={key} className="mb-4">
                 <button
                   disabled={key === chatRoomId}
                   //@ts-ignore
                   title={`Join ${chatRooms[key].name}`}
-                  onClick={() => handleJoinedRoom(key)}>
+                  onClick={() => handleJoinedRoom(key)}
+                  className={`
+                    bg-white px-4 py-2 rounded-lg shadow-md focus:outline-none
+                    ${key === chatRoomId ? "bg-gray-300 cursor-default" : ""}
+                  `}>
                   {/* @ts-ignore */}
                   {chatRooms[key].name}
                 </button>
@@ -43,15 +46,15 @@ function ChatRooms() {
           })}
         </ul>
       </div>
-      <div className="flex gap-2 mb-5">
+      <div className="flex flex-col gap-2 mb-5">
         <input
           placeholder="Chat Room Name"
           ref={newChatRoomRef}
-          className="focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+          className="focus:outline-none focus:placeholder-gray-400 text-gray-700 placeholder-gray-500 px-4 py-2 rounded-lg bg-gray-100 shadow-md"
         />
         <button
           onClick={handleChatting}
-          className="font-bold inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+          className="font-bold inline-flex items-center justify-center rounded-lg px-6 py-2 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none shadow-md">
           Start Chatting
         </button>
       </div>
