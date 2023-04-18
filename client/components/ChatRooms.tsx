ef31a9681d8c9bfb3d8e37b7af0140c059d8a19c
highpriesst <ertansofia@gmail.com>
2023-04-18 17:31:25 +0300

    Styling and adding some little features.

--- client/components/ChatRooms.tsx
+++ client/components/ChatRooms.tsx
@@ -23,30 +23,8 @@ function ChatRooms() {
   }
 
   return (
-    <div className="flex flex-row gap-10">
-      <div className="border h-max bg-gray-100 p-4 rounded-lg">
-        <ul>
-          {Object.keys(chatRooms).map((key) => {
-            return (
-              <div key={key} className="mb-4">
-                <button
-                  disabled={key === chatRoomId}
-                  //@ts-ignore
-                  title={`Join ${chatRooms[key].name}`}
-                  onClick={() => handleJoinedRoom(key)}
-                  className={`
-                    bg-white px-4 py-2 rounded-lg shadow-md focus:outline-none
-                    ${key === chatRoomId ? "bg-gray-300 cursor-default" : ""}
-                  `}>
-                  {/* @ts-ignore */}
-                  {chatRooms[key].name}
-                </button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="flex flex-col gap-2 mb-5">
+    <div className="flex flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center justify-center gap-2 mb-5">
         <input
           placeholder="Chat Room Name"
           ref={newChatRoomRef}
@@ -57,6 +35,30 @@ function ChatRooms() {
           className="font-bold inline-flex items-center justify-center rounded-lg px-6 py-2 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none shadow-md">
           Start Chatting
         </button>
+      </div>
+
+      <div className="border h-max bg-gray-100 p-4 rounded-lg">
+        <h2 className="text-lg font-medium mb-2 text-center">Chat Rooms</h2>
+        <ul className="max-h-48 overflow-y-auto">
+          {Object.keys(chatRooms).map((key) => {
+            return (
+              <li key={key} className="mb-2">
+                <button
+                  disabled={key === chatRoomId}
+                  //@ts-ignore
+                  title={`Join ${chatRooms[key].name}`}
+                  onClick={() => handleJoinedRoom(key)}
+                  className={`
+              w-full bg-white px-4 py-2 rounded-lg shadow-md focus:outline-none
+              ${key === chatRoomId ? "bg-gray-300 cursor-default" : ""}
+            `}>
+                  {/* @ts-ignore */}
+                  {chatRooms[key].name}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
--- client/components/Texts.tsx
+++ client/components/Texts.tsx
@@ -1,65 +1,122 @@
 import { useSockets } from "@/context/socketContext";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function Texts() {
   const { socket, messages, chatRoomId, username, setMessages } = useSockets();
 
   const messagesRef = useRef(null);
 
-  function handleMessages() {
-    //@ts-ignore
-    const message = messagesRef.current.value;
+  const messageEndRef = useRef(null);
+  //My working solution
+  // function handleMessages() {
+  //   //@ts-ignore
+  //   const message = messagesRef.current.value;
 
-    if (!String(message).trim()) {
+  //   if (!String(message).trim()) {
+  //     return;
+  //   }
+
+  //   socket.emit("send-message", { chatRoomId, message, username });
+
+  //   const date = new Date();
+
+  //   setMessages([
+  //     //@ts-ignore
+  //     ...messages,
+  //     {
+  //       username: "You",
+  //       message,
+  //       time: `${date.getHours}:${date.getMinutes}`,
+  //     },
+  //   ]);
+  //   //@ts-ignore
+  //   messagesRef.current.value = "";
+  // }
+
+  function handleMessages(): void {
+    const message = messagesRef.current?.value?.trim();
+    if (!message) {
       return;
     }
 
     socket.emit("send-message", { chatRoomId, message, username });
 
     const date = new Date();
+    const formattedTime = `${date.getHours()}:${date.getMinutes()}`;
 
-    setMessages([
-      //@ts-ignore
-      ...messages,
+    setMessages((prevState) => [
+      ...prevState,
       {
         username: "You",
         message,
-        time: `${date.getHours}:${date.getMinutes}`,
+        time: formattedTime,
       },
     ]);
-    //@ts-ignore
-    messagesRef.current.value = "";
+
+    messagesRef.current!.value = "";
   }
+
+  useEffect(() => {
+    //@ts-ignore
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   if (!chatRoomId) {
     return <div></div>;
   }
 
   return (
-    <div className="border flex flex-col items-center h-full gap-10 justify-evenly">
-      <div className="border">
-        {/* @ts-ignore */}
-        {messages.map(({ message, username }, index) => {
-          return (
-            <div key={index}>
-              <div key={index}>
-                <span>{username}</span>
-                <span>{message}</span>
-              </div>
-            </div>
-          );
-        })}
-        <div />
+    <div className="border flex flex-col items-center h-full gap-10 justify-evenly wrapper">
+      <div className="border flex-1 overflow-y-scroll max-h-full w-full messagesList">
+        {/* If there are no messages */}
+        {messages.length === 0 ? (
+          <div className="h-full flex flex-col items-center justify-center text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-16 w-16 mb-4"
+              viewBox="0 0 20 20"
+              fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M5.293 6.293a1 1 0 011.414 0L10 8.586l3.293-3.293a1 1 0 111.414 1.414L11.414 10l3.293 3.293a1 1 0 01-1.414 1.414L10 11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586 10 5.293 6.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <p className="text-lg font-medium">No messages yet</p>
+          </div>
+        ) : (
+          // If there are messages
+          <div className="messageList flex-1 overflow-y-scroll p-4">
+            {messages.map(({ message, username, time }, index) => {
+              return (
+                <div
+                  key={index}
+                  className="message mb-4 p-4 rounded-lg border border-gray-400 bg-gray-100">
+                  <div key={index} className="messageInner">
+                    <span className="messageSender text-xs font-medium text-gray-500">
+                      {username} - {time}
+                    </span>
+                    <span className="messageBody break-words text-sm ml-2">
+                      {message}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+            {/* This is used to scroll to the bottom of the messages */}
+            <div ref={messageEndRef} />
+          </div>
+        )}
       </div>
-      <div className="w-60 flex gap-2">
+      <div className="w-full flex gap-2">
         <input
           placeholder="Type a message"
           ref={messagesRef}
-          className="focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+          className="focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 w-full"
         />
         <button
           onClick={handleMessages}
-          className="font-bold inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+          className="font-medium inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
           SEND
         </button>
       </div>
